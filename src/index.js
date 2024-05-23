4b2d6d3f93a590568689ec2244d5bfe5b61f1397
MrArthor <vksvs25@gmail.com>
2024-05-23 15:07:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { identifyContact } = require('./identify');
-const app = express();
 const testConnection = require('./test-db-connection');
 testConnection();
-app.use(bodyParser.json());
+const app = express();
 
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Server is running' });
+});
 app.post('/identify', identifyContact);
+
+const PORT = process.env.PORT || 3000; // Default to port 3000 if not specified in .env
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
